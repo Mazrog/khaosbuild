9806bf3d272c7da36d782237cb14c4d1d250a03e
mazrog <maxime.cabanal@outlook.fr>
2018-12-18 21:51:51 +0100

    [Cosmetic] cmake configuration now show what folder will be copied

--- common.cmake
+++ common.cmake
@@ -5,7 +5,7 @@ if ( DEFINED ENV{KHAOS_BUILD} )
     set ( CMAKE_ARCHIVE_OUTPUT_DIRECTORY "$ENV{KHAOS_BUILD}/lib/${CMAKE_BUILD_TYPE}" )
     set ( CMAKE_RUNTIME_OUTPUT_DIRECTORY "$ENV{KHAOS_BUILD}/bin/${CMAKE_BUILD_TYPE}" )
 
-    include_directories(BEFORE SYSTEM "$ENV{KHAOS_BUILD}/include/${CMAKE_BUILD_TYPE}")
+    include_directories(AFTER SYSTEM "$ENV{KHAOS_BUILD}/include/${CMAKE_BUILD_TYPE}")
     link_directories(BEFORE SYSTEM "$ENV{KHAOS_BUILD}/lib/${CMAKE_BUILD_TYPE}")
 
     macro(publish_files target_name)
@@ -14,7 +14,7 @@ if ( DEFINED ENV{KHAOS_BUILD} )
             file( COPY "${CMAKE_CURRENT_SOURCE_DIR}/${target_name}/${PUBLISHED_FILES}"
                 DESTINATION "$ENV{KHAOS_BUILD}/include/${CMAKE_BUILD_TYPE}/"
             )
-            message ( "Copying file to Runtime directory: $ENV{KHAOS_BUILD}/include/${CMAKE_BUILD_TYPE}/" )
+            message ( "Copying folder ${PUBLISHED_FILES} to runtime directory: $ENV{KHAOS_BUILD}/include/${CMAKE_BUILD_TYPE}/" )
             unset( PUBLISHED_FILES )
         endif()
     endmacro(publish_files)
